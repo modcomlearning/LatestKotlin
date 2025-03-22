2fad4fbce631d5270212c5fb26816ac415af7243
modcomlearning <modcomlearning@gmail.com>
2025-03-22 21:00:27 +0300

    Final

--- Day1/Lesson1a.kt
+++ Day1/Lesson1a.kt
@@ -1,16 +1,16 @@
 fun main() {
-    //Here we do not specify the Data Types
-    //In this Case the Data Type is identified by Its value
-    // we will differentaite between val and var and Focus on val only
+    //Using val to declare/define and initialize variables.
+    //Here, we do not specify the data type.
+    //Kotlin automatically recognizes the Datatype
     val myNum = 5             // Int
     val myDoubleNum = 5.99    // Double
     val myLetter = 'D'        // Char
     val myBoolean = true      // Boolean 
     val myText = "Hello"      // String
+
     print(myText)
     print(myNum)
     print(myDoubleNum)
     print(myLetter)
     print(myBoolean)
-    //NB: You can change the variable names if you wish
 }
