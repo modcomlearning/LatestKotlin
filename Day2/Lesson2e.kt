00f6f78c3098064a98287378096e758dd4062a10
modcomlearning <modcomlearning@gmail.com>
2025-03-22 09:21:54 +0300

    Final

--- Day2/Lesson2e.kt
+++ Day2/Lesson2e.kt
@@ -1,6 +1,6 @@
 fun main() {
     var marks: Int = 90 // we set some marks here
-
+    //Explain that when is an alternative to IF Statements
     when (marks) {
         in 1..30 -> println("You have $marks - Below average")
         in 31..50 -> println("You have $marks - Average")
